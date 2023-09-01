416be297794068fc212b1c800cf551b3e5ea15f9
Valeriy <masterbox06@gmail.com>
2023-09-01 17:29:04 +0400

    added profile layout

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,11 +1,80 @@
 import styled from "styled-components";
+import {Post} from "./Post/Post.tsx";
+import {OwnerAvatarExampleURL, ProfileNewPostTextareaPlaceHolder} from "../../globalVars.ts";
 
 export const Profile = () => {
   return (
-    <ProfileWrapper></ProfileWrapper>
+    <ProfileWrapper>
+      <ProfileInfoWrapper>
+        <Avatar
+          src={OwnerAvatarExampleURL}
+          alt=""
+        />
+        <DescriptionWrapper>
+          <h1>Dmitry</h1>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </DescriptionWrapper>
+      </ProfileInfoWrapper>
+      <PostsWrapper>
+        <h2>My posts</h2>
+
+        <NewPostWrapper>
+          <textarea rows={2} placeholder={ProfileNewPostTextareaPlaceHolder}></textarea>
+          <button>Send</button>
+        </NewPostWrapper>
+
+        <PostsList>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+        </PostsList>
+      </PostsWrapper>
+    </ProfileWrapper>
   );
 };
 
 const ProfileWrapper = styled.div`
-  background-color: crimson;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
+const ProfileInfoWrapper = styled.div`
+  display: flex;
+`
+const Avatar = styled.img`
+  width: 8rem;
+  height: 8rem;
+  object-fit: cover;
+  border-radius: 100%;
+  border: solid ${p => p.theme.text} .25rem;
+`
+const DescriptionWrapper = styled.div`
+  margin-left: 2rem;
+`
+const PostsWrapper = styled.div``
+const NewPostWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+  gap: .5rem;
+  margin-top: 1.5rem;
+
+  textarea {
+    width: 100%;
+    padding: .5rem;
+    box-sizing: border-box;
+  }
+;
+
+  button {
+    font-size: 1.125rem;
+    line-height: 1.75rem;
+    padding: .25rem 2rem;
+  }
+`
+const PostsList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
 `
