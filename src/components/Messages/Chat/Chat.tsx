c004393f0ce48f5135aa4efc6bfb7ca53ade5e31
Valeriy <masterbox06@gmail.com>
2023-09-03 13:24:17 +0400

    added chat component

---
+++ src/components/Messages/Chat/Chat.tsx
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+import {UserType} from "../../../globalVars.ts";
+import {Message} from "./Message/Message.tsx";
+
+type PropsType = {
+  user: UserType
+}
+
+export const Chat = ({user}: PropsType) => {
+  return (
+    <ChatWrapper>
+      <ChatHeader>
+        {user.name}
+      </ChatHeader>
+      <ChatBody>
+        <Message user={user} me/>
+        <Message user={user}/>
+        <Message user={user}/>
+        <Message user={user} me/>
+        <Message user={user}/>
+      </ChatBody>
+      <ChatInputWrapper>
+        <textarea></textarea>
+        <button>Send</button>
+      </ChatInputWrapper>
+    </ChatWrapper>
+  );
+};
+
+const ChatWrapper = styled.div`
+  height: 100%;
+  border-radius: 0 0 0.5rem 0.5rem;
+`
+const ChatHeader = styled.div`
+  height: 4rem;
+  border-bottom: solid ${p => p.theme.primary50} 2px;
+
+  background-color: ${p => p.theme.secondary};
+`
+const ChatBody = styled.div`
+  max-height: 100%;
+  padding: 2rem;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  //overflow-y: scroll;
+
+  background-color: ${p => p.theme.background};
+`
+const ChatInputWrapper = styled.div`
+  height: 4rem;
+  display: flex;
+  align-items: center;
+
+  background-color: ${p => p.theme.background};
+
+  textarea {
+    height: 100%;
+    width: 100%;
+    border: none;
+  }
+
+  button {
+    height: 2rem;
+    margin: 1rem;
+    padding: .5rem 1rem;
+    box-sizing: border-box;
+  }
+`
