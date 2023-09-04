ee698112789ec62f602a956b45ac485c98d5202a
Valeriy <masterbox06@gmail.com>
2023-09-04 13:53:08 +0400

    Logo wrapped with NavLink, added cursor pointer style to logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 import reactLogo from "../../assets/react.svg";
+import {NavLink} from "react-router-dom";
 
 export const Header = () => {
   return (
     <HeaderWrapper>
-      <Logo src={reactLogo} alt="React logo"/>
+      <NavLink to="/profile">
+        <Logo src={reactLogo} alt="React logo"/>
+      </NavLink>
       <h3>ACME</h3>
 
     </HeaderWrapper>
@@ -14,7 +17,6 @@ export const Header = () => {
 const HeaderWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  background-color: coral;
   grid-column-start: 1;
   grid-column-end: 3;
   align-items: center;
@@ -24,4 +26,5 @@ const HeaderWrapper = styled.div`
 const Logo = styled.img`
   width: 3rem;
   height: 3rem;
+  cursor: pointer;
 `
