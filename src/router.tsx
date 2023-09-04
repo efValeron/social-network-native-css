450283b3c44012add68964f3b90705b1f81fd80a
Valeriy <masterbox06@gmail.com>
2023-09-04 13:52:20 +0400

    deleted router file

--- src/router.tsx
+++
@@ -1,9 +0,0 @@
-import {createBrowserRouter} from "react-router-dom";
-import {Profile} from "./components/Profile/Profile.tsx";
-
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Profile/>,
-  },
-]);
